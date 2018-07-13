0d76784d3773784c1be278f0a6d28f02cb1f1c60
Robert Ying <yingrui205@gmail.com>
2018-07-13 14:38:49 +0800

    Fix display output

--- display.v
+++ display.v
@@ -1,13 +1,9 @@
 module display(
     input digi_in,
-    output digi_out_1,
-    output digi_out_2,
-    output digi_out_3，
-    output digi_out_4);
+    output [7:0] digi_out,
+    output [3:0] AN);
 
-    assign digi_out_1 = digi_in[11:8] == 4'b0001? digi_in[7:0]: 7'b0;
-    assign digi_out_2 = digi_in[11:8] == 4'b0010? digi_in[7:0]: 7'b0;
-    assign digi_out_3 = digi_in[11:8] == 4'b0100? digi_in[7:0]: 7'b0;
-    assign digi_out_4 = digi_in[11:8] == 4'b1000? digi_in[7:0]: 7'b0;
+    assign digi_out = digi_in[7:0];
+    assign AN = digi_in[11:8];
 
 endmodule
