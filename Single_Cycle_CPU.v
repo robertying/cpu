76049a33dac2ae1f7fba48b3174d88da45983e62
Deng Chenghao <deng4869@icloud.com>
2018-07-13 09:28:35 +0800

    single update

--- Single_Cycle_CPU.v
+++ Single_Cycle_CPU.v
@@ -1,5 +1,4 @@
-
-module Single_CPU (clk, reset, led, digi, UART_RX, UART_TX);
+module Single_Cycle_CPU (clk, reset, led, digi, UART_RX, UART_TX);
 input clk;
 input reset;
 input UART_RX;
@@ -38,6 +37,7 @@ wire [1:0] RegDst;
 wire RegWr;
 wire ALUSrc1;
 wire ALUSrc2;
+wire [5:0] ALUFun;
 wire Sign;
 wire MemWr;
 wire MemRd;
@@ -48,15 +48,16 @@ wire LUOp;
 assign PC_4 = PC +3'h4;
 assign ConBA = PC_4 + {Imm32[29:0], 2'b00};
 
-assign JT = Instruction[25:0];
+assign JT = {PC_4[31:28], Instruction[25:0], 2'b00};
 assign Imm16 = Instruction[15:0];
 assign Shamt = Instruction[10:6];
 assign Rd = Instruction[15:11];
 assign Rt = Instruction[20:16];
 assign Rs = Instruction[25:21];
 
-assign ALU_In_A = ALUSrc1? Shamt:DataBusA;
-assign ALU_In_B = ALUSrc2? (LUOp? {Imm16, 16'b0}:Imm32):DataBusB;
+assign ALU_In_A = ALUSrc1 ? Shamt : DataBusA;
+assign ALU_In_B = ALUSrc2 ? (LUOp ? {Imm16, 16'b0} : Imm32) : DataBusB;
+assign Imm32 = (ExtOp && Imm16[15] ) ? {16'hffff, Imm16} : {16'h0000, Imm16};
 
 always @(*)
 	case (RegDst)
@@ -78,7 +79,7 @@ always @(*)
 always @(*)
 	case (PCSrc)
 		3'b000: PC_next <= PC_4;
-		3'b001: PC_next <= ALUOut[0]? ConBA:PC_4;
+		3'b001: PC_next <= ALUOut[0] ? ConBA : PC_4;
 		3'b010: PC_next <= JT;
 		3'b011: PC_next <= DataBusA;
 		3'b100: PC_next <= ILLOP;
@@ -86,15 +87,21 @@ always @(*)
 		default: PC_next <= XADR;
 	endcase
 
-ROM instruction_memory(.addr(PC[30:0]), .data(Instruction)));
-Control control(.Instuct(Instruction), .IRQ(IRQ), .PC31(PC[31]),
-	.PCSrc(PCSrc), .RegDst(RegDst), .RegWr(RegWr), .ALUSrc1(ALUSrc1), .ALUSrc2(ALUSrc2),
-	.ALUFun(ALUFun), .Sign(Sign), .MemWr(MemWr), .MemRd(MemRd), .MemToReg(MemToReg), 
-	.ExtOp(ExtOp), .LUOp(LUOp));
+always @(posedge clk or negedge reset) begin
+	if(~reset)
+		PC <= 32'h0000_0000;
+	else
+		PC <= PC_next;
+end
+
+ROM instruction_memory(.addr(PC[30:0]), .data(Instruction));
+Control control(.Instruct(Instruction), .IRQ(IRQ), .PC31(PC[31]),
+				.PCSrc(PCSrc), .RegDst(RegDst), .RegWr(RegWr), .ALUSrc1(ALUSrc1), .ALUSrc2(ALUSrc2),
+				.ALUFun(ALUFun), .Sign(Sign), .MemWr(MemWr), .MemRd(MemRd), .MemToReg(MemToReg), 
+				.ExtOp(ExtOp), .LUOp(LUOp));
 RegFile regfile(.reset(reset), .clk(clk), .addr1(Rs), .data1(DataBusA),
-	.addr2(Rt), .data2(DataBusB), .wr(RegWr), .addr3(Rc), .data3(DataBusC));
+				.addr2(Rt), .data2(DataBusB), .wr(RegWr), .addr3(Rc), .data3(DataBusC));
 ALU alu(.A(ALU_In_A), .B(ALU_In_B), .ALUFun(ALUFun), .Sign(Sign), .OUT(ALUOut));
-DataMem datamem(.reset(reset), .clk(clk), .rd(MemRd), .wr(MemWr), .addr(ALUOut), .wdata(DataBusB), rdata(MemOut));
-Peripheral peripheral(.reset(reset), .clk(clk), .rd(), .wr(), .addr(), .wdata(), .rdata(), .led(led), .switch(), .digi(digi), .irqout(IRQ));
-UART uart(.reset(reset), .clk(clk), )
+DataMem datamem(.reset(reset), .clk(clk), .rd(MemRd), .wr(MemWr), .addr(ALUOut), .wdata(DataBusB), .rdata(MemOut));
+//Peripheral peripheral(.reset(reset), .clk(clk), .rd(), .wr(), .addr(), .wdata(), .rdata(), .led(led), .switch(), .digi(digi), .irqout());
 endmodule
